2ebf4988fdda0ad34fcd386bdd7ac9853cad54b0
Roy Kathurima <kathurimaroy@gmail.com>
2025-03-26 10:23:00 +0300

    Added annotations to the service class, methods and exposed types

--- app/src/main/java/org/example/models/Models.java
+++ app/src/main/java/org/example/models/Models.java
@@ -1,11 +1,15 @@
 package org.example.models;
 
 import java.util.List;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 public class Models {
+
+    @XmlRootElement
     public record Client(int id, String name, String email) {
     }
 
+    @XmlRootElement
     public record Room(int id, int room_number, double price_per_night, boolean booked) {
     }
 
@@ -15,6 +19,7 @@ public class Models {
     public record ReservedRoom(int room_number, double price_per_night) {
     }
 
+    @XmlRootElement
     public record Reservation(String name, String email, double total_price, List<ReservedRoom> rooms) {
     }
 }
--- app/src/main/java/org/example/service/BantuLodgeService.java
+++ app/src/main/java/org/example/service/BantuLodgeService.java
@@ -3,6 +3,9 @@ package org.example.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.jws.WebService;
+import jakarta.jws.WebMethod;
+
 // local imports
 import org.example.dao.Database;
 import org.example.models.Models.Client;
@@ -11,25 +14,31 @@ import org.example.models.Models.SingleReservation;
 import org.example.models.Models.Reservation;
 import org.example.models.Models.ReservedRoom;
 
+@WebService
 public class BantuLodgeService {
 
     // Service method to add client
+    @WebMethod
     public Client addClient(String name, String email) {
         return Database.addClient(name, email);
     }
 
+    @WebMethod
     public Room addRoom(Integer room_number, Double nightly_price) {
         return Database.addRoom(room_number, nightly_price);
     }
 
+    @WebMethod
     public List<Room> getAvailableRooms() {
         return Database.getAvailableRooms();
     }
 
+    @WebMethod
     public static Room bookRoom(Integer id) {
         return Database.bookRoom(id);
     }
 
+    @WebMethod
     public static Reservation getBookedRoomsForUser(String email) {
         List<SingleReservation> reservations = Database.getBookedRoomsForUser(email);
         String name = null;
