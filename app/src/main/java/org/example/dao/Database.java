b62722f49c5072b87b1d9ecdff63a6cf720021cb
Roy Kathurima <kathurimaroy@gmail.com>
2025-03-26 09:38:38 +0300

    Added model class with definitions of data shape. Updated the Database queries to return the correct types

--- app/src/main/java/org/example/dao/Database.java
+++ app/src/main/java/org/example/dao/Database.java
@@ -1,54 +1,68 @@
 package org.example.dao;
 
 import org.jdbi.v3.core.Jdbi;
+// local imports
+import org.example.models.Models.Room;
+
+import java.util.List;
+
+import org.example.models.Models.Client;
+import org.example.models.Models.SingleReservation;
 
 // statuc makes the class act like a utility class
 public class Database {
     private static final Jdbi jdbi = Jdbi.create("url", "db_name", "db_password");
 
     // Add a user (client)
-    // TODO: Should return the created user
-    public static void addClient(String name, String email) {
-        jdbi.useHandle(handle -> {
-            handle.execute("INSERT INTO clients (name, email) VALUES (?, ?);", name, email);
+    public static Client addClient(String name, String email) {
+        return jdbi.withHandle(handle -> {
+            return handle.createUpdate("INSERT INTO clients (name, email) VALUES (:name, :email) RETURNING *;")
+                    .bind("name", name)
+                    .bind("email", email)
+                    .executeAndReturnGeneratedKeys()
+                    .mapTo(Client.class)
+                    .one();
         });
     }
 
     // Add rooms as an admin possibly
-    // TODO: should return the created room
-    public static void addRoom(Integer room_number, Double nightly_price) {
-        jdbi.useHandle(handle -> {
-            handle.execute("INSERT INTO room (room_number, price_per_night) VALUES (?, ?);", room_number,
-                    nightly_price);
+    public static Room addRoom(Integer room_number, Double nightly_price) {
+        return jdbi.withHandle(handle -> {
+            return handle.createUpdate(
+                    "INSERT INTO room (room_number, price_per_night) VALUES (:room_number, :nightly_price) RETURNING *;")
+                    .bind("room_number", room_number)
+                    .bind("nightly_price", nightly_price)
+                    .executeAndReturnGeneratedKeys()
+                    .mapTo(Room.class)
+                    .one();
         });
     }
 
     // return a list of available rooms
-    // TODO: Does not return anything yet; add a record(data class)
-    public static void getAvailableRooms() {
-        jdbi.useHandle(handle -> {
-            handle.execute("SELECT * FROM room WHERE booked = FALSE;");
+    public static List<Room> getAvailableRooms() {
+        return jdbi.withHandle(handle -> {
+            return handle.createQuery("SELECT * FROM room WHERE booked = FALSE;")
+                    .mapToBean(Room.class)
+                    .list();
         });
     }
 
     // Book room
-    // TODO: Should return the booked room
-    // Not sure if the id is an int or a string, guess well finf out soon
-    public static void bookRoom(Integer id) {
-        jdbi.useHandle(handle -> {
-            handle.execute("UPDATE room SET booked = TRUE WHERE id = ?;", id);
-        });
+    public static Room bookRoom(Integer id) {
+        return jdbi.withHandle(handle -> handle.createQuery("UPDATE room SET booked = TRUE WHERE id = :id RETURNING *;")
+                .bind("id", id)
+                .mapTo(Room.class)
+                .one());
     }
 
     // Get a list of booked rooms
     // the service that will call this class will handle the logic of adding up the
     // totals
-    // TODO: Should return a list of all the booked rooms for a given user (id user
-    // by email)
-    public static void getBookedRoomsForUser(String email) {
-        jdbi.useHandle(handle -> {
-            handle.execute("SELECT r.* FROM reservedroom r INNER JOIN client c ON c.id = r.client_id WHERE c.email = ?",
-                    email);
-        });
+    public static List<SingleReservation> getBookedRoomsForUser(String email) {
+        return jdbi.withHandle(handle -> handle.createQuery(
+                "SELECT r.id, c.email, c.name, rm.room_number, rm.price_per_night FROM reservedroom r INNER JOIN client c ON c.id = r.client_id INNER JOIN room rm ON rm.id = r.room_id WHERE c.email = :email;")
+                .bind("email", email)
+                .mapTo(SingleReservation.class)
+                .list());
     }
 }
--- app/src/main/java/org/example/models/Models.java
+++ app/src/main/java/org/example/models/Models.java
@@ -1,5 +1,6 @@
-
 package org.example.models;
+
+import java.util.List;
 
 public class Models {
     public record Client(int id, String name, String email) {
@@ -8,6 +9,12 @@ public class Models {
     public record Room(int id, int room_number, double price_per_night, boolean booked) {
     }
 
-    public record Reservation(int id, int client_id, int room_id) {
+    public record SingleReservation(int id, String name, String email, int room_number, double price_per_night) {
+    }
+
+    public record ReservedRoom(int room_number, double price_per_night) {
+    }
+
+    public record Reservation(String name, String email, double total_price, List<ReservedRoom> rooms) {
     }
 }
