7e59ce68a4295d9a324efa971cc90e2f5c7e5720
Roy Kathurima <kathurimaroy@gmail.com>
2025-03-26 10:15:41 +0300

    Add a service to add clients and rooms, get available rooms, book oom, get booked rooms for a user

---
+++ app/src/main/java/org/example/service/BantuLodgeService.java
@@ -0,0 +1,49 @@
+package org.example.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// local imports
+import org.example.dao.Database;
+import org.example.models.Models.Client;
+import org.example.models.Models.Room;
+import org.example.models.Models.SingleReservation;
+import org.example.models.Models.Reservation;
+import org.example.models.Models.ReservedRoom;
+
+public class BantuLodgeService {
+
+    // Service method to add client
+    public Client addClient(String name, String email) {
+        return Database.addClient(name, email);
+    }
+
+    public Room addRoom(Integer room_number, Double nightly_price) {
+        return Database.addRoom(room_number, nightly_price);
+    }
+
+    public List<Room> getAvailableRooms() {
+        return Database.getAvailableRooms();
+    }
+
+    public static Room bookRoom(Integer id) {
+        return Database.bookRoom(id);
+    }
+
+    public static Reservation getBookedRoomsForUser(String email) {
+        List<SingleReservation> reservations = Database.getBookedRoomsForUser(email);
+        String name = null;
+        double totalCost = 0;
+        List<ReservedRoom> reservedRooms = new ArrayList<>();
+        for (SingleReservation reservation : reservations) {
+            totalCost += reservation.price_per_night();
+            if (name == null) {
+                name = reservation.name();
+            }
+            ReservedRoom reservedRoom = new ReservedRoom(reservation.room_number(), reservation.price_per_night());
+            reservedRooms.add(reservedRoom);
+        }
+        assert name != null : "We should have a name by now";
+        return new Reservation(name, email, totalCost, reservedRooms);
+    }
+}
